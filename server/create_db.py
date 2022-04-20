c357db267cf10ce018a6405476548dd882b668ba
Ani Khachatryan <69533525+ani-khachatryan@users.noreply.github.com>
2022-04-20 22:01:58 +0300

    Fixed db

--- server/create_db.py
+++ server/create_db.py
@@ -1,26 +1,28 @@
 import sqlite3
 
 
-connect = sqlite3.connect('database')
+connect = sqlite3.connect('database.db')
+connect.execute("PRAGMA foreign_keys = 1")
 curs = connect.cursor()
-
 curs.execute('''
             CREATE TABLE IF NOT EXISTS users
-            ([user_id] INTEGER NOT NULL IDENTITY PRIMARY KEY,
+            ([user_id] INTEGER PRIMARY KEY NOT NULL,
             [name] TEXT,
             [username] TEXT NOT NULL UNIQUE,
             [password] TEXT NOT NULL,
             [telegram_id] TEXT NOT NULL UNIQUE)
             ''')
 
+connect.commit()
 curs.execute('''
             CREATE TABLE IF NOT EXISTS tasks
-            ([task_id] INTEGER NOT NULL IDENTITY PRIMARY KEY,
-            FOREIGN KEY(user_id) REFERENCES users(user_id) NOT NULL,
+            ([task_id] INTEGER PRIMARY KEY NOT NULL,
+            [user_id] INTEGER NOT NULL,
             [description] TEXT NOT NULL,
             [date] DATE NOT NULL)
             ''')
 
-curs.commit()
+            #FOREIGN KEY([user_id]) REFERENCES users([user_id]),
+connect.commit()
 curs.close()
 connect.close()
