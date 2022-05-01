f790916c2c2b4c9ace0d0b12b203607a74f606c6
Ani Khachatryan <69533525+ani-khachatryan@users.noreply.github.com>
2022-05-01 14:59:48 +0300

    minor fix

---
+++ server/server.py
@@ -0,0 +1,50 @@
+import socket
+from database import RequestHandler
+import threading
+import requests
+
+HEADER = 64
+PORT = 5050
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+SERVER = s.getsockname()[0]
+s.close()
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "exit"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            else:
+                print(f"[{addr}] {msg}")
+                result  = RequestHandler(msg)
+                for row in result:
+                    conn.send(row.encode(FORMAT))
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
