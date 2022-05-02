1830f9019c6c0518bc2e60255b0d90bb3c61d665
Involid <karagulian.mv@phystech.edu>
2022-05-02 03:41:21 +0300

    some fixes

--- client/main.py
+++ client/main.py
@@ -7,10 +7,13 @@ from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
 import os
+#from kivy.uix.listview import ListItemButton
 import client
+
 
 Builder.load_file('login.kv')
 Builder.load_file('registration.kv')
+#Builder.load_file('tasks.kv')
 
 class LoginWindow(Screen):
     username = ObjectProperty(None)
@@ -18,10 +21,13 @@ class LoginWindow(Screen):
     def validate(self):
         self.user = client.login(self.username.text, self.password.text)
         if len(self.user) == 1:
-            print('chulala')
+            pass
+            #invalid login
         else:
-            print('ulala')
-        print(type(self.user))
+            pass
+            #login
+            self.manager.current = 'tasks'
+        print(self.user)
 
     def do_login(self, loginText, passwordText):
         app = App.get_running_app()
@@ -38,14 +44,27 @@ class RegistrationWindow(Screen):
     password = ObjectProperty(None)
 
     def signupbtn(self):
-        pass
+        self.user = client.registration(self.username.text, 
+                                        self.username.text,
+                                        self.password.text, 
+                                        self.email.text)
+        print(type(self.user))
+        print(self.user)
+        #self.user is ok ????
+        self.manager.current = 'login'
+
+class TaskScreen(Screen):
+    pass
+    #Tasks = client.get_tasks()
+    #Tasks = ['anasun', 'ailandak muxam', 'kulisneri poshi']
 
 class LoginApp(App):
     def build(self):
         manager = ScreenManager()
 
         manager.add_widget(LoginWindow(name='login'))
-        manager.add_widget(RegistrationWindow(name='signup')) 
+        manager.add_widget(RegistrationWindow(name='signup'))
+        manager.add_widget(TaskScreen(name='tasks'))
         return manager
 
 if __name__ == '__main__':
